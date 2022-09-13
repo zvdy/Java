289625d17f4c336d8f3d4151abfb589ef8d3fc5f
zvdy <cristian.030902@gmail.com>
2022-09-13 13:57:45 +0200

    twosum

---
+++ twosum/twosum.java
@@ -0,0 +1,10 @@
+package twosum;
+// java hello world
+public class twosum {
+    public static void main(String[] args) {
+        int num1 = 23; //First int to sum
+        int num2 = 91; //Second int to sum
+        int sum = num1 + num2; //Sum operation
+        System.out.println(sum); //Print the result of sum operation
+    }
+}
