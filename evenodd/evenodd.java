6536ef12d1887903f5fcaa0e6be8d7d7fe873497
zvdy <cristian.030902@gmail.com>
2022-09-14 23:20:54 +0200

    even odd num checker

---
+++ evenodd/evenodd.java
@@ -0,0 +1,14 @@
+package evenodd;
+
+public class evenodd {
+    //check if number is even or odd
+    public static void main(String[] args) {
+        int number = 5;
+        if (number % 2 == 0) {
+            System.out.println("The number is even");
+        }
+        else {
+            System.out.println("The number is odd");
+        }
+    }
+}
