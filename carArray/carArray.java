95b2bb8800dac9f84f6bd95064cf04d658ea2e51
zvdy <cristian.030902@gmail.com>
2022-09-15 22:38:24 +0200

    car array loop

---
+++ carArray/carArray.java
@@ -0,0 +1,12 @@
+package carArray;
+
+public class carArray {
+    public static void main(String[] args) {
+    // We create an Array to store all the car brands
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Audi"};
+    // for loop to iterate trough the loop and print out the content
+    for (int i = 0; i < cars.length; i++) {
+        System.out.println("Cars inside array: " + cars[i]);
+        }
+    }
+}
