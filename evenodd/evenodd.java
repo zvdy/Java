a04c5978ad157bd7bd78b74e4fe60f296c6ef64e
zvdy <cristian.030902@gmail.com>
2022-09-15 10:34:10 +0200

    added comments

--- evenodd/evenodd.java
+++ evenodd/evenodd.java
@@ -3,10 +3,13 @@ package evenodd;
 public class evenodd {
     //check if number is even or odd
     public static void main(String[] args) {
+        // we create the variable number
         int number = 5;
+        // do a if where we check the remainder of dividing our variable to 2, so we can determinate if it is even or odd
         if (number % 2 == 0) {
             System.out.println("The number is even");
         }
+        // if the var isn't divisible by 2 we print out 'odd'
         else {
             System.out.println("The number is odd");
         }
--- zerototen/zerototen.java
+++ zerototen/zerototen.java
@@ -2,6 +2,7 @@ package zerototen;
 
 public class zerototen {
     public static void main(String[] args) {
+        // we make a for loop so we can iterate our variable 'i' until it fulfills our condition 
         for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
