bcd17dbe3dcb046c8b2ad37992ee12e3f4728296
zvdy <cristian.030902@gmail.com>
2022-09-19 13:20:46 +0200

    jokey java script to print a spanish joke

--- joke/joke.java
+++ joke/joke.java
@@ -3,10 +3,14 @@ package joke;
 
 public class joke {
     public static void main(String[] args) {
+        // we do a for loop to iterate from 1 to 100
         for (int i = 1; i <= 100; i++) {
-        if (i % 5 == 0 && i % 2 != 0) {
+        // We create a if statement to return a print if i 
+        // is divisible only by 5 so we can output a rhyme
+            if (i % 5 == 0 && i % 2 != 0) {
             System.out.println(i +" Agarramela que me crece");
         }
+        // we print the rest of the numbers
         else {
             System.out.println(i);
         }
