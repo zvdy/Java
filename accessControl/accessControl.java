7245705972c17955f34a2363f7ac2878013c8280
zvdy <cristian.030902@gmail.com>
2022-09-19 23:04:31 +0200

    Access Control to class given a password

---
+++ accessControl/accessControl.java
@@ -0,0 +1,21 @@
+package accessControl;
+
+import java.util.Scanner;
+public class accessControl {
+    public static void main(String[] args) {
+        // we ask the user to enter the password and we save the user input
+        System.out.print("Please enter the password: ");
+        Scanner keyboard = new Scanner(System.in);
+        String password = keyboard.nextLine();
+        // we check if the password is correct
+        if (password.equals("S")) {
+            System.out.println("Access granted to SMR");
+        } else if (password.equals("D")) {
+            System.out.println("Access granted to DAW");
+        } else if (password.equals("M")) {
+            System.out.println("Access granted to DAM");
+        } else {
+            System.out.println("Access denied"); 
+        }
+    }
+}
