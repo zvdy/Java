093d924d75267698cb0beedc03d777012e1a4903
zvdy <cristian.030902@gmail.com>
2022-09-20 13:58:20 +0200

    basic user information display

---
+++ infoUser/infoUser.java
@@ -0,0 +1,38 @@
+package infoUser;
+
+import java.util.Scanner;
+
+public class infoUser {
+
+    public static void main(String[] args) {
+        // We create user inputs for Name, Surname, ID, Sex and age
+        String name;
+        String surname;
+        String ID;
+        String sex;
+        String age;
+        // We ask the user for the inputs
+        System.out.print("Enter your name: ");
+        Scanner kb = new Scanner(System.in);
+        name = kb.nextLine();
+        System.out.print("Enter your surname: ");
+        Scanner kb0 = new Scanner(System.in);
+        surname = kb0.nextLine();
+        System.out.print("Enter your ID: ");
+        Scanner kb1 = new Scanner(System.in);
+        ID = kb1.nextLine();
+        System.out.print("Enter your Sex: ");
+        Scanner kb2 = new Scanner(System.in);
+        sex = kb2.nextLine();
+        System.out.print("Enter your age: ");
+        Scanner kb3 = new Scanner(System.in);
+        // We print out the results on a user friendly way
+        age = kb3.nextLine();
+        System.out.println("Your name is: " + name);
+        System.out.println("Your surname is: " + surname);
+        System.out.println("Your ID is: " + ID);
+        System.out.println("Your Sex is: " + sex);
+        System.out.println("Your age is: " + age);
+
+    }
+}
