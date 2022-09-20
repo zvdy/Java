f86c8769839734e574bbc0cccd8a247b849dac7f
zvdy <cristian.030902@gmail.com>
2022-09-20 14:05:55 +0200

    display user initials given the 1st and last name

---
+++ initials/initials.java
@@ -0,0 +1,15 @@
+package initials;
+
+import java.util.Scanner;
+public class initials {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // We ask the user for the first name and lastname
+        System.out.print("Enter your first name: ");
+        String firstName = input.next();
+        System.out.print("Enter your last name: ");
+        String lastName = input.next();
+        // We print out the initials using CharAt method
+        System.out.println("Your initials are: " + firstName.charAt(0) + lastName.charAt(0));
+    }
+}
