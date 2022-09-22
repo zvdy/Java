2cd4ecbdee3ac6ac4e7fd479a4a7d3488bfb1c32
zvdy <cristian.030902@gmail.com>
2022-09-22 10:35:51 +0200

    Java palindrome number checker

---
+++ palindromeNumber/palindromeNumber.java
@@ -0,0 +1,25 @@
+package palindromeNumber;
+
+import java.util.Scanner;
+
+public class palindromeNumber {
+    public static void main(String[] args) {
+        int x;
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        x = kb.nextInt();
+        int z = x;
+        int y = 0;
+        while (x > 0) {
+            y = y * 10 + x % 10;
+            x = x / 10;
+        }
+        if (z == y) {
+            System.out.println("The number is a palindrome");
+        } else {
+            System.out.println("The number is not a palindrome");
+        }
+        
+            
+    }
+}
