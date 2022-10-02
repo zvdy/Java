dcdf39da0f79f899a3f8e365de1bb399bd0a1242
zvdy <cristian.030902@gmail.com>
2022-10-02 20:37:46 +0200

    Java Password generator

---
+++ encrypt/encrypt/encrypt.java
@@ -0,0 +1,18 @@
+package encrypt;
+
+public class encrypt {
+    public static void main(String[] args) {
+        // Ask user for a password
+        System.out.println("Enter a password: ");
+        String password = System.console().readLine();
+        // Encrypt the password
+        String encrypted = "";
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            c = (char) (c + 2 - (i + 1) -2);
+            encrypted = encrypted + c;
+        }
+        // Print the encrypted password
+        System.out.println("Encrypted password: " + encrypted);
+    }
+}
