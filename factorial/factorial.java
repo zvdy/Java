a83edb13a06c5f718a6c30a5efb3ba8bae50d93d
zvdy <cristian.030902@gmail.com>
2022-10-03 22:16:32 +0200

    Java factorial number

---
+++ factorial/factorial.java
@@ -0,0 +1,16 @@
+package factorial;
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        // Ask a user fot a number and return the factorial of that number
+        System.out.println("Enter a number: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        System.out.println("The factorial of " + number + " is " + factorial);
+
+    }
+}
